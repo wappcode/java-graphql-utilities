b2ac956ef36b5add065663e7e69d148fd6f56331
Jesus Abarca <jesus.abarca.g@gmail.com>
2025-02-16 12:15:09 -0600

    Parent with modules initial application configuration

---
+++ graphql-utilities/src/main/java/com/wappcode/java/graphql/utilities/MyLibrary.java
@@ -0,0 +1,7 @@
+package com.wappcode.java.graphql.utilities;
+
+public class MyLibrary {
+    public String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
---
+++ src/test/java/com/wappcode/java/graphql/GraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wappcode.java.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
