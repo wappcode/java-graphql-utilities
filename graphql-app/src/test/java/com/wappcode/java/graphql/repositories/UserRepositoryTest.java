f393b32f048ea5b332900d602d66d2ce65a285c0
Jesus Abarca <jesus.abarca.g@gmail.com>
2025-02-16 15:04:04 -0600

    Prueba inicial test con repositorio

---
+++ graphql-app/src/test/java/com/wappcode/java/graphql/repositories/UserRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.wappcode.java.graphql.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testUserRepository() {
+        assertTrue(userRepository != null);
+    }
+}
